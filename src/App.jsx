746683309ff741b79f12a3df62bd579feaf610eb
William R. Stetar <kebekad673@proton.me>
2024-08-08 11:35:35 -0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export const App = () => {
+  return (
+    <>
+      {""}
+      <div>
+        <h1>Hello</h1>
+        <div>Welcome to your first React Application!</div>
+      </div>
+    </>
+  );
+};
