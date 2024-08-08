586282dda7141c4d5337484d8148a96b578be03e
William R. Stetar <kebekad673@proton.me>
2024-08-08 14:41:55 -0500

    Added Count

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+// App.jsx
+import { useState } from "react";
+
 export const App = () => {
+
+const [count, setCount] = useState(0)
+
+  const handlebuttonclick = () => {
+  setCount (count + 1)
+  console.log("clicked me ouch!")
+  }
+
   return (
     <>
       {""}
       <div>
         <h1>Hello!! :: </h1>
         <div>Welcome to your first React Application!</div>
+        <button onClick={handlebuttonclick} className="btn-secondary">CLICK ME IF YOU DARE</button>
+        <div>COUNT {count}</div>
       </div>
     </>
   );
