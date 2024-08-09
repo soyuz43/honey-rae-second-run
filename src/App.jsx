db259d2d336c5226b0a00dec75eeabc8650cdc3a
William R. Stetar <kebekad673@proton.me>
2024-08-09 13:19:32 -0500

    Tickets JSX Created

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,25 @@ export const App = () => {
         <button onClick={handlebuttonclick} className="btn-secondary">CLICK ME IF YOU DARE</button>
         <div>COUNT {count}</div>
       </div>
+      <div className="tickets-container">
+        <h2>Tickets</h2>
+        <article className="tickets">
+          {allTickets.map(ticket => {
+            return (
+              <section className="ticket">
+                <header className="ticket-info"> #{ticket.id}</header>
+                <div>{ticket.description}</div>
+                <footer>
+                  <div>
+                    <div className="ticket-info">emergency</div>
+                    <div>{ticket.emergency ? "yes" : "no" }</div>
+                  </div>
+                </footer>
+              </section>
+            )
+          })}
+        </article>
+      </div>
     </>
   );
 };
