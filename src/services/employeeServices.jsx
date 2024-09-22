85c6bfdb2d471f92297094303f40ac6b5e71d272
William R. Stetar <kebekad673@proton.me>
2024-09-21 19:16:09 -0500

    Temp Commit

--- src/services/employeeServices.jsx
+++ src/services/employeeServices.jsx
@@ -1,27 +1,29 @@
 export const getAllEmployees = () => {
-    return fetch(`http://localhost:8088/employees?_expand=user`).then((res) => res.json()
-)
-}
+  return fetch(`http://localhost:8088/employees?_expand=user`).then((res) =>
+    res.json()
+  );
+};
 export const getEmployeeByUserId = async (employeeId) => {
-    try {
-      const response = await fetch(`http://localhost:8088/employees?userId=${employeeId}&_expand=user&_embed=employeeTickets`);
-      const data = await response.json();
-      
-      console.log('Employee Data:', data); 
-      return data;
-    } catch (error) {
-      console.error(error);
-      return [];
-    }
-  };
+  try {
+    const response = await fetch(
+      `http://localhost:8088/employees?userId=${employeeId}&_expand=user&_embed=employeeTickets`
+    );
+    const data = await response.json();
+
+    console.log("Employee Data:", data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
 
 export const updateEmployee = (employee) => {
-    return fetch(`http://localhost:8088/employees/${employee.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(employee),
-    });
-  };
-  
+  return fetch(`http://localhost:8088/employees/${employee.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(employee),
+  });
+};
